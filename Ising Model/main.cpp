37fe37483bb675c7029c76f20895398d19cbf5f6
Yiming Yu <yu.yiming.self@gmail.com>
2022-02-21 08:48:39 -0600

    Update main.cpp

--- Ising Model/main.cpp
+++ Ising Model/main.cpp
@@ -2,19 +2,45 @@
 #include <string>
 
 #include "ising_model.hpp"
+#include "repl.hpp"
+
+static Ising gs_model;
 
 [[noreturn]]
-void repl(Ising& config) {
-    std::cout << "REPL started" << '\n';
+void repl() {
+    println("REPL started.");
+    prompt();
     std::string line;
     
     while (std::getline(std::cin, line)) {
-        
+        if (line == k_exit) {
+            std::cout << std::endl;
+            std::exit(0);
+        }
+        auto view = line | stdv::split(' ')
+                         | stdv::transform([](auto&& range) { return std::string_view(&*range.begin(), std::ranges::distance(range)); })
+                         | stdv::filter([](auto&& sv) { return sv != ""; });
+        std::vector<std::string_view> command(view.begin(), view.end());
+
+        if (command.size() == 0) {
+            print_usage();
+            continue;
+        }
+
+        // init <spins_file> <bond_file>
+        if (command[0] == k_init) {
+           if (command.size() != 3) {
+               print_usage();
+               continue;
+           }
+           // TODO
+        }
+        else if (command[0] == k_hist) {
+            
+        }
     }
 }
 
 int main() {
-    auto config = make_ising("spin.txt", "bond.txt");
-    repl(config);
     return 0;
 }
