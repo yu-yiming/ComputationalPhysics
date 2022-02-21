d2db763ab73ba56fa2ae6d178f5f723ec7e6266f
Yiming Yu <yu.yiming.self@gmail.com>
2022-02-20 20:12:35 -0600

    Update main.cpp

--- Ising Model/main.cpp
+++ Ising Model/main.cpp
@@ -1,5 +1,20 @@
+#include <iostream>
+#include <string>
+
 #include "ising_model.hpp"
 
+[[noreturn]]
+void repl(Ising& config) {
+    std::cout << "REPL started" << '\n';
+    std::string line;
+    
+    while (std::getline(std::cin, line)) {
+        
+    }
+}
+
 int main() {
+    auto config = make_ising("spin.txt", "bond.txt");
+    repl(config);
     return 0;
 }
