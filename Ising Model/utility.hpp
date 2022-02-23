8aebb844160c24327c94b4139e9f01e12696547f
Yiming Yu <yu.yiming.self@gmail.com>
2022-02-22 18:50:55 -0600

    Create utility.hpp

---
+++ Ising Model/utility.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <random>
+#include <type_traits>
+
+/**
+ * @brief Generate a random number within a give range.
+ * 
+ * @tparam T Either a integral type or a floating piont type.
+ * @param left The left bound of the range, inclusive.
+ * @param right The right bound of the range, exclusive.
+ * @return T A random number.
+ */
+template<typename T, typename U, typename R = std::common_type_t<T, U>>
+R randnum(T left, U right) {
+    using distribution_t = std::conditional_t<std::is_integral_v<R>, 
+                                              std::uniform_int_distribution<R>, std::uniform_real_distribution<R>>;
+
+    std::random_device rd{};
+    std::default_random_engine eng(rd());
+    distribution_t d(left, right);
+
+    return d(eng);
+}
