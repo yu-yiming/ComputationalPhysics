b5cad3a7af2df4cbb7251c8636e356d4e3c87aad
Yiming Yu <yu.yiming.self@gmail.com>
2022-03-06 23:10:16 -0600

    Update utility.hpp

--- Ising Model/utility.hpp
+++ Ising Model/utility.hpp
@@ -30,3 +30,128 @@ R randnum(T left, U right) {
 
     return result;
 }
+
+//template<typename T, typename U>
+//struct cons;
+//
+//template<typename T, typename... Args>
+//struct cons<T, std::tuple<Args...>> {
+//    using type = std::tuple<T, Args...>;
+//};
+//
+//template<typename T, typename U>
+//using cons_t = typename cons<T, U>::type;
+//
+//template<typename T, typename U>
+//struct in;
+//
+//template<typename T>
+//struct in<T, std::tuple<>> {
+//    using type = std::false_type;
+//};
+//
+//template<typename T, typename... Tail>
+//struct in<T, std::tuple<T, Tail...>> {
+//    using type = std::true_type;
+//};
+//
+//template<typename T, typename Head, typename... Tail>
+//struct in<T, std::tuple<Head, Tail...>> {
+//    using type = typename in<T, Tail...>::type;
+//};
+//
+//template<typename T, typename U>
+//using in_t = typename in<T, U>::type;
+//
+//template<typename... Args>
+//struct intersect;
+//
+//template<typename... Args>
+//struct intersect<std::tuple<>, std::tuple<Args...>> {
+//    using type = std::false_type;
+//};
+//
+//template<typename Head, typename... Tail, typename... Args>
+//struct intersect<std::tuple<Head, Tail...>, std::tuple<Args...>> {
+//    using type = std::disjunction<in_t<Head, std::tuple<Args...>>, typename intersect<std::tuple<Tail...>, std::tuple<Args...>>::type>;
+//};
+//
+//template<typename... Args>
+//using intersect_t = typename intersect<Args...>::type;
+//
+//template<template<class...> class Pred, typename T, typename... Args>
+//struct filter;
+//
+//template<template<class...> class Pred, typename... Args>
+//struct filter<Pred, std::tuple<>, Args...> {
+//    using type = std::tuple<>;
+//};
+//
+//template<template<class...> class Pred, typename Head, typename... Tail, typename... Args>
+//struct filter<Pred, std::tuple<Head, Tail...>, Args...> {
+//    using type = std::conditional_t<Pred<Head, Args...>,
+//        cons_t<Head, typename filter<Pred, std::tuple<Tail...>>::type>,
+//        typename filter<Pred, std::tuple<Tail...>>::type>;
+//};
+//
+//template<template<class> class Pred, typename T, typename... Args>
+//using filter_t = typename filter<Pred, T, Args...>::type;
+//
+//template<template<typename...> class C, typename D>
+//struct rebind;
+//
+//template<template<typename...> class C, template<typename...> class D, typename... Args>
+//struct rebind<C, D<Args...>> {
+//    using type = C<Args...>;
+//};
+//
+//template<template<typename...> class C, typename D>
+//using rebind_t = typename rebind<C, D>::type;
+
+template<typename T>
+struct transpose {};
+
+template <typename... Ts>
+struct transpose<std::tuple<std::vector<Ts>...>> {
+    using type = std::vector<std::tuple<Ts...>>;
+};
+
+template <typename... Ts>
+struct transpose<std::vector<std::tuple<Ts...>>> {
+    using type = std::tuple<std::vector<Ts>...>;
+};
+
+template<typename... Ts>
+using transpose_t = typename transpose<Ts...>::type;
+
+template<typename... Ts>
+static inline std::tuple<Ts&...> ith(size_t i, std::vector<Ts>&... vs) {
+    return std::tie(vs[i]...);
+}
+
+template<typename... Ts>
+class Builder {
+public:
+    Builder(std::tuple<Ts const&...> const& args)
+        : m_tuple(args) {}
+
+    std::tuple<Ts const&...>&& tuple() const {
+        return std::move(m_tuple);
+    }
+
+    template<typename T>
+    auto operator ,(Builder const& other) {
+        return Builder(std::tuple_cat(m_tuple, other.m_tuple));
+    }
+
+private:
+    std::tuple<Ts const&...> m_tuple;
+};
+
+template<typename... Ts>
+Builder(std::tuple<Ts&...> const&) -> Builder<Ts...>;
+
+template<typename... T>
+std::tuple<T const&...> ctie(T const&... args) {
+    return std::tie(args...);
+}
